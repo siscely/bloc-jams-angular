a17895a3d228fa23cd2e655833a598ee12017df0
siscely george <siscely.george@gmail.com>
2017-08-30 12:44:27 -0700

    commit changes

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,42 +1,70 @@
 (function() {
     function SongPlayer() {
          var SongPlayer = {};
+
+         /**
+* @desc private object to hold the current song
+* @type {Object}
+*/
          var currentSong = null;
          /**
 * @desc Buzz object audio file
 * @type {Object}
 */
          var currentBuzzObject = null;
+
+         var playSong = function(song){
+           currentBuzzObject.play();
+           song.playing = true;
+         }
          /**
           * @function setSong
           * @desc Stops currently playing song and loads new audio file as currentBuzzObject
           * @param {Object} song
           */
          var setSong = function(song) {
-           if (currentBuzzObject) {
+            if (currentBuzzObject) {
              currentBuzzObject.stop();
              currentSong.playing = null;
-           }
+            }
 
-      currentBuzzObject = new buzz.sound(song.audioUrl, {
-      formats: ['mp3'],
-      preload: true
-    });
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+              formats: ['mp3'],
+              preload: true
+            });
 
-    currentSong = song;
- };
+            currentSong = song;
+          };
+
+ /**
+  * @method SongPlayer.play
+  * @desc Public method that stops currently playing song and loads new audio file as currentBuzzObject
+  * @param {Object} song
+  */
+
          SongPlayer.play = function(song) {
            if (currentSong !== song) {
              setSong(song);
-            currentBuzzObject.play();
-       } else if (currentSong === song) {
-         if (currentBuzzObject.isPaused()) {
+             playSong(song);
+            }
+           else if (currentSong === song) {
+             if (currentBuzzObject.isPaused()) {
              currentBuzzObject.play();
-             song.playing = true;
-         }
-     }
-     };
-         return SongPlayer;
+              }
+          }
+       };
+  /**
+  * @method SongPlayer.pause
+  * @desc Public method that stops currently playing song
+  * @param {Object} song
+  */
+
+
+       SongPlayer.pause = function(song) {
+         currentBuzzObject.pause();
+         song.playing = false;
+        };
+      return SongPlayer;
     }
 
     angular
